a58e36bbfc918596f9740c9aef4b20e604801f0b
David-Yuba <dado2012yuba@gmail.com>
2024-06-10 10:24:37 +0200

    algebra hell and experimentation

--- javascript.js
+++ javascript.js
@@ -18,39 +18,29 @@ let resUniformLocation = ctx.getUniformLocation(prog, "res");
 ctx.uniform2f(resUniformLocation, canvas.width, canvas.height);
 
 let radUniformLocation = ctx.getUniformLocation(prog, "rad");
-
+let length=1;
 
 let position = [
     0,0,
-    0.7,0.7,
-    -0.7,0.7,
-    -0.7,-0.7,
-    0.7,-0.7,
-    0.7,0.7,
+    length,length,
+    -length,length,
+    -length,-length,
+    length,-length,
+    length,length,
 ]
 let primitiveType = ctx.TRIANGLE_FAN;
 let offset = 0;
 let count = 6;
 
-let duration = 4000, sign=1;
 
 requestAnimationFrame(breath);
 function breath(time){
+
     ctx.clear(ctx.COLOR_BUFFER_BIT);
     ctx.bufferData(ctx.ARRAY_BUFFER, new Float32Array(position), ctx.STATIC_DRAW);
-
-    if(time>duration){
-        duration= duration+4000;
-        sign*=-1
-    }
-    else if(sign==1){
-        ctx.uniform1f(radUniformLocation, ((time%4000)/20000)*sign);
-    }
-    else if(sign==-1){
-        ctx.uniform1f(radUniformLocation, ((-4000+(time%4000))/20000)*sign);
-    }
+    ctx.uniform1f(radUniformLocation, time);
+    
 
     ctx.drawArrays(primitiveType, offset, count);
-    console.log(duration);
     requestAnimationFrame(breath);
 }
